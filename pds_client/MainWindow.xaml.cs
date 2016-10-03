5cda4316403f243768cefb441117d3f9011bdeb8
Davide Visentin <davide.visentin@studenti.polito.it>
2016-10-03 13:40:52 +0200

    Prova grafica

--- pds_client/MainWindow.xaml.cs
+++ pds_client/MainWindow.xaml.cs
@@ -12,17 +12,44 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Threading;
+using pds_client;
 
 namespace pds_client
 {
-    /// <summary>
-    /// Logica di interazione per MainWindow.xaml
-    /// </summary>
-    public partial class MainWindow : Window
-    {
-        public MainWindow()
-        {
-            InitializeComponent();
-        }
-    }
+	/// <summary>
+	/// Logica di interazione per MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window
+	{
+		private Dictionary<string, Server> Servers = new Dictionary<string, Server>();
+		private string aggiornamenti=null;
+
+		public MainWindow()
+		{
+			InitializeComponent();
+		}
+
+		private void ConnettiServer(object sender, RoutedEventArgs e)
+		{
+			try
+			{
+				Server S = new Server(textBox.Text, 1993, new DownloadCallback(UsaAggiornamenti));
+				Servers.Add(textBox.Text, S);
+				Thread Downloader = new Thread(S.ScaricaAggiornamenti);
+				Downloader.Start(aggiornamenti);
+				MessageBox.Show("Connessione riuscita");
+                serverList.Items.Add(S);
+			}
+			catch (Exception exception)
+			{
+                MessageBox.Show("Impossibile connettersi a questo server: "+exception.Message);
+			}
+		}
+
+		private void UsaAggiornamenti(string data)
+		{
+			
+		}
+	}
 }
---
+++ pds_client/Server.xaml.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Net;
+using System.Net.Sockets;
+
+public delegate void DownloadCallback(string data);
+
+namespace pds_client
+{
+	/// <summary>
+	/// Logica di interazione per Server.xaml
+	/// </summary>
+	public partial class Server : TabItem
+	{
+		int Port;
+		TcpClient Socket = new TcpClient();
+		DownloadCallback CallbackDownload;
+
+		public string IpString
+		{
+			get { return (string)GetValue(IpStringProperty); }
+			set { SetValue(IpStringProperty, value); }
+		}
+
+		// Using a DependencyProperty as the backing store for EntityName.  This enables animation, styling, binding, etc...
+		public static readonly DependencyProperty IpStringProperty =
+			DependencyProperty.Register("IpString", typeof(string), typeof(Server), new PropertyMetadata(""));
+
+		public Server()
+		{
+			InitializeComponent();
+		}
+
+		public Server(string ipString)
+		{
+			IpString = ipString;
+			InitializeComponent();
+		}
+
+		public Server(string ipString, int port, DownloadCallback callbackDownload)
+		{
+			IpString = ipString;
+			Port = port;
+			Socket.Connect(IPAddress.Parse(ipString), port);
+			CallbackDownload = callbackDownload;
+			InitializeComponent();
+		}
+
+		public void ScaricaAggiornamenti()
+		{
+			Byte[] Data = System.Text.Encoding.ASCII.GetBytes("");
+			while (true)
+			{
+				((NetworkStream)Socket.GetStream()).Read(Data, 0, 1024);
+				if (CallbackDownload == null)
+					throw new Exception("Callback per gestire gli aggiornamenti non registrata");
+				CallbackDownload(System.Text.Encoding.ASCII.GetString(Data));
+			}
+		}
+	}
+}
